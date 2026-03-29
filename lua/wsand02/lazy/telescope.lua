bdf932885a143d798ae6e373a7c73ebf9c208f0f
William Sandbrink <29465936+wsand02@users.noreply.github.com>
2026-03-29 18:20:49 +0200

    telescope binds

--- lua/wsand02/lazy/telescope.lua
+++ lua/wsand02/lazy/telescope.lua
@@ -5,5 +5,16 @@ return {
 
     dependencies = {
         "nvim-lua/plenary.nvim"
-    }
+    },
+
+    config = function()
+        require("telescope").setup({})
+
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>ps', function()
+            builtin.grep_string({search = vim.fin.input("Grep > ") })
+        end)
+    end
 }
--- lua/wsand02/remap.lua
+++ lua/wsand02/remap.lua
@@ -1,3 +1,4 @@
 
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+print("Hello, from wsand02")
