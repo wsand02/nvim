5ef95a338351edb6f709b170ae2fbaafd4489c29
William Sandbrink <29465936+wsand02@users.noreply.github.com>
2026-03-30 15:48:10 +0200

    add lsp configs

--- lua/wsand02/init.lua
+++ lua/wsand02/init.lua
@@ -1,3 +1,4 @@
 require("wsand02.remap")
 require("wsand02.set")
 require("wsand02.lazy_init")
+require("wsand02.lsp_init")
---
+++ lua/wsand02/lsp_init.lua
@@ -0,0 +1,6 @@
+vim.lsp.enable("gopls")
+vim.lsp.enable("pyright")
+vim.lsp.enable("rust_analyzer")
+vim.lsp.enable("ts_ls")
+vim.lsp.enable("lua_ls")
+
