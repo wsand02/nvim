33523bd8d552598cabbb73166a7abd7bf51d64f2
William Sandbrink <29465936+wsand02@users.noreply.github.com>
2026-05-02 12:35:05 +0200

    add gd etc
    cant believe i never got around to this

--- lua/wsand02/init.lua
+++ lua/wsand02/init.lua
@@ -1,3 +1,22 @@
 require("wsand02.remap")
 require("wsand02.set")
 require("wsand02.lazy_init")
+
+local augroup = vim.api.nvim_create_augroup
+local WsandGroup = augroup('wsand02', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd('LspAttach', {
+    group = WsandGroup,
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    end
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
